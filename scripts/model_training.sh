bb692c75a135c869a4721d95142f62f592e82639
Dustin Miao <Dustin.Miao@jax.org>
2025-06-30 09:01:54 -0400

    minor bugfixes to model training script and adjusted time+memory bounds

--- scripts/model_training.sh
+++ scripts/model_training.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 #SBATCH --job-name=model_training
-#SBATCH --time=12:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
 #SBATCH --qos=long
 #SBATCH --partition=compute
-#SBATCH --mem=128G
+#SBATCH --mem=64G
 #SBATCH --output=/projects/kumar-lab/miaod/projects/unsupervised-aging/logs/output-%j.txt
 
 PROJECT_DIR="/projects/kumar-lab/miaod/projects/unsupervised-aging"
@@ -17,8 +17,8 @@ python "${PROJECT_DIR}/src/model_training.py" \
     --xcat_json   "${PROJECT_DIR}/data/2025-06-28_supervised-unsupervised-xcats.json" \
     --output_path "${PROJECT_DIR}/data/2025-06-28_model-training-results.csv" \
     --seed 623 \
-    --outer_n_splits 50 \
-    --inner_n_splits 10 \
+    --outer_n_splits 10 \
+    --inner_n_splits 5 \
     --cpu_cores $SLURM_CPUS_ON_NODE \
     --X_cats unsup sup all \
     --y_cats age fi
